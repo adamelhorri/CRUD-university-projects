8be5e3edb122c53099f8606f14b42121c3818863
Mohammed FARAJI <637-fm223902@users.noreply.www-apps.univ-lehavre.fr>
2023-02-17 18:51:40 +0100

    Classe créée : AbstractEntite

---
+++ Entites/AbstractEntite.php
@@ -0,0 +1,30 @@
+<?php
+
+abstract class AbstractEntite
+{
+  private bool $persistant;
+
+  /**
+   * Get the value of persistant
+   *
+   * @return boolean
+   */
+  public function getPersistant(): bool
+  {
+    return $this->persistant;
+  }
+
+  /**
+   * Set the value of persistant
+   *
+   * @param boolean $persistant
+   * @return self
+   */
+  public function setPersistant(bool $persistant): self
+  {
+    $this->persistant = $persistant;
+    return $this;
+  }
+}
+
+?>

a37946455af2910cf1594cdb8e1f03f9f3a75dca
AKRAM DOUBLI <600-da204275@users.noreply.www-apps.univ-lehavre.fr>
2023-02-24 21:03:51 +0100

    Classe créée : AbstractVueRelaion

---
+++ Vues/AbstractVueRelation.php
@@ -0,0 +1,35 @@
+<?php
+
+class AbstractVueRelation
+{
+    /**
+     *
+     * @return string
+     */
+    public function getDebutHTML(): string
+    {
+        return "<!DOCTYPE html>
+        <html lang='fr'>
+<head>
+<meta charset='UTF-8'>
+<meta name='viewport' content='width=device-width, initial-scale=1'/>
+<title>CRUD-Series-télévisées</title>
+<link rel='stylesheet' href='global.css' />
+</head>
+<body> ";
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getFintHTML(): string
+    {
+        return "
+</body>
+</html>\n";;
+    }
+
+}
+
+?>

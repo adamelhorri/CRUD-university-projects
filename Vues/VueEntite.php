8899e0f28853eb0f4a55644887bae1b7259a98f5
Mohammed FARAJI <637-fm223902@users.noreply.www-apps.univ-lehavre.fr>
2023-03-03 18:27:02 +0100

    Classe créée : VueEntite

---
+++ Vues/VueEntite.php
@@ -0,0 +1,30 @@
+<?php
+
+abstract class VueEntite extends AbstractVueRelation
+{
+  /**
+   * getHTML4Entity
+   *
+   * @param  AbstractEntite $entite
+   * @return string
+   */
+  public abstract function getHTML4Entity(AbstractEntite $entite = null): string;
+    
+  /**
+   * getAllEntities
+   *
+   * @param  array $tabEntities
+   * @return string
+   */
+  public abstract function getAllEntities(array $tabEntities): string;
+  
+  /**
+   * getForme4Entity
+   *
+   * @param  AbstractEntite $entite
+   * @return string
+   */
+  public abstract function getForme4Entity(AbstractEntite $entite = null): string;
+}
+
+?>
